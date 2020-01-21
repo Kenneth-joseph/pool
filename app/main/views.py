0336ab131c69d9ff44b87c4c85aad614874a2eaf
jakesIII <jayruma@yahoo.com>
2020-01-21 20:42:38 +0300

    Added navbar in the html

--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,11 @@
 from flask import render_template,redirect,url_for   #Takes in the name of the template.
-from app import app
+
 from . import main
 
 
 from flask_bootstrap import Bootstrap
 
-
-#Views
-@app.route('/Home')
-def home():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('navbar.html')
-
-
 @main.route('/')
 def index():
 
-    return render_template('index.html')
+    return render_template('navbar.html')
