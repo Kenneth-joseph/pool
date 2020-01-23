8ef83c0de659dcf4b59514d0e17f95fd303f3af7
trevor ngugi <ngugitrevor@gmail.com>
2020-01-23 15:10:57 +0300

    configured the upload dets in config .py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://trevor:elite@localhost/pool'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
